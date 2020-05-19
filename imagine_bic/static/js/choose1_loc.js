183266a2eb1d23d79008609a8c973beb58f910d2
South_under_been <s2018s27@e-mirim.hs.kr>
2020-05-19 10:10:15 +0900

    choose_loc(not-finiished)

--- imagine_bic/static/js/choose1_loc.js
+++
@@ -1,6 +0,0 @@
-function bkfun(){
-    location.href="/index";
-}
-function sm(){
-    lanForm.submit();
-}
---
+++ imagine_bic/static/js/choose_loc.js
@@ -0,0 +1,13 @@
+function bkfun(){
+    location.href="/index";
+}
+
+function onmdono(){
+    
+}
+
+function oncdono(){
+    var bg=document.getElementById('whitemap');
+    bg.src="../img/a.png";
+    lanForm.submit();
+}
