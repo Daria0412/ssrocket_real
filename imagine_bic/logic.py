750f6b1bfe8aba33bb8167d101f4565fe27cfff3
daria0412 <s2018w29@icloud.com>
2020-05-21 00:52:08 +0900

    semi clean logic

--- imagine_bic/logic.py
+++ imagine_bic/logic.py
@@ -1,63 +1,197 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from django.http import HttpResponse, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
 from django.utils.translation import ugettext_lazy as _
 from django.utils import translation
-
+import datetime
 class Sign:
     def __init__(self):
         pass
 
     def signin(request, id,pwd):#로그인
         users = User.objects.filter(id = id)
-        response = render(request, 'imagine_bic/login.html',{"okay":1,"users":users})
         if users is not None:
             for user in users:
                 if id == user.id and pwd == user.pwd:
+                    request.session['id']=id
+                    request.session['user_info']=user.info
                     if user.info == 0:
-                        response = render(request, 'imagine_bic/index.html',{"okay":1,"users":users})
+                        return render(request, 'imagine_bic/index.html',{"okay":1,"users":users})
                     else:
-                        response = render(request,'imagine_bic/company_main.html',{"count":2,"users":users})
-                    response.set_cookie('id',id)
-                    response.set_cookie('user_info',user.info)
+                        return render(request,'imagine_bic/company_main.html',{"count":2,"users":users})
         else : 
-            response = HttpResponse("<html><script>alert('로그인 오류입니다. 다시 시도해주세요');location.href='signin';</script></html>")
-        return response
+            return HttpResponse("<html><script>alert('로그인 오류입니다. 다시 시도해주세요');location.href='signin';</script></html>")
+        return render(request, 'imagine_bic/login.html',{"okay":1,"users":users})
     
     def signup(request,user_info): #회원가입
-        Sign.trans(request)
         id = request.POST['id']
         name = request.POST['name']
         pwd = request.POST['pwd']
         user = User.objects.create(id = id, name = name, pwd = pwd, info = user_info)
         if user_info == "0":
             return render(request, 'imagine_bic/login.html')
-        response = render(request, 'imagine_bic/signup2_company.html',{"count":1})
-        response.set_cookie('id',id)
-        return response
+        request.session['id']=id
+        return render(request, 'imagine_bic/signup2_company.html',{"count":1})
 
-    def translate(request):
-        lan = request.COOKIES.get('lan')
-        if lan == "":
-            lan = request.POST['lan']
-            request.set_cookie('lan',lan)
-        if lan == 'ko' or lan == 'en' or lan == 'jp':
-            if translation.LANGUAGE_SESSION_KEY in request.session:
-                del(request.session[translation.LANGUAGE_SESSION_KEY])
-            translation.activate(lan)
-            request.session[translation.LANGUAGE_SESSION_KEY] = f'{lan}'
-        else:
-            if translation.LANGUAGE_SESSION_KEY in request.session:
-                del(request.session[translation.LANGUAGE_SESSION_KEY])
-            translation.activate('en')
-            request.session[translation.LANGUAGE_SESSION_KEY] = 'ko'
-    
-    def trans(request):
-        lan = request.COOKIES.get('lan')
-        Sign.translate(request)
+    def signup_company(request):
+        user_info =request.session['user_info']
+        id = request.session['id']
+        users = User.objects.filter(id = id)
+        for user in users:
+            name = user.name
+        if request.method == "POST":
+            company_addr = request.POST['company_addr']
+            company_loc = request.POST['company_loc']
+            company_phone = request.POST['company_phone']
+            bicycle_num = request.POST['bicycle_num']
+            s_business = request.POST['s_business']
+            e_business = request.POST['e_business']
+            company_info = request.POST['company_info']
+            company = Company.objects.create(member_name = name, company_addr = company_addr,company_phone=company_phone,bicycle_num=bicycle_num,member_id=id, s_business=s_business,e_business=e_business,company_info=company_info,company_loc=company_loc,rent_num=0)
+            request.session.clear()
+            return render(request, 'imagine_bic/login.html',{"count":1})
+        return render(request, 'imagine_bic/signup2_company.html')
 
 class Choose:
-    def lan(request):
-        Sign.translate(request)   
-        response = render(request, 'imagine_bic/choose_use.html',{"count":1})
-        return response
+    def __init__(self):
+        pass
+
+    def index(request):
+        id = request.session['id'] 
+        users = User.objects.filter(id = id)
+        for user in users :
+            user_info = user.info
+        if user_info == 0:
+            return render(request, 'imagine_bic/index.html', {'users':users,"count":1})
+        return render(request, 'imagine_bic/company_main.html',{'users':users,"count":1})
+
+    def choose_lan(request):
+        if request.method == "POST":
+            request.session['lan'] = request.POST['lan']
+            return render(request, 'imagine_bic/choose_use.html',{"count":1})
+        return render(request, 'imagine_bic/choose_lan.html')
+
+    def choose_loc(request):
+        id = request.session['id'] 
+        users = User.objects.filter(id = id)
+        if request.method == "POST":
+            request.session['company_loc']=request.POST['search']
+            return redirect('/choose_shop')
+        return render(request, 'imagine_bic/choose_loc.html',{"users":users})
+
+    def choose_shop(request):
+        company_loc = request.session['company_loc'] 
+        companys = Company.objects.filter(company_loc = company_loc)
+        return render(request, 'imagine_bic/choose_shop.html',{"companys":companys})
+
+    def choose_time(request):
+        id = request.session['id'] 
+        company_loc=request.session['company_loc']
+        company_num = request.session['company_num']
+        users = User.objects.filter(id = id)
+        for user in users:
+            name = user.name
+        companys = Company.objects.filter(company_num = company_num)
+        for company in companys:
+            shop = company
+        if request.method == "POST":
+            print("post")
+            r_year = request.POST['r_year']
+            r_month = request.POST['r_month']
+            r_date = request.POST['r_date']
+            r_b = request.POST['r_btime']
+            r_r = request.POST['r_rtime']
+            r_btime = "{}-{}-{} {}".format(r_year,r_month,r_date,r_b)#datetime formatting
+            r_rtime = "{}-{}-{} {}".format(r_year,r_month,r_date,r_r)
+            btime = "1970-01-02 00:00:00"
+            rtime = "1970-01-02 00:00:00"
+            #db insert
+            history = History.objects.create(company_num=company_num, member_id = id, r_btime = r_btime, r_rtime = r_rtime, reserved_At = datetime.datetime.now(), btime =btime, rtime = rtime)
+            for company in companys:
+                Company.objects.update(rent_num=company.rent_num+1)
+            return render(request, 'imagine_bic/choose_end.html',{"name":name,"history_num":history.history_num})
+        return render(request, 'imagine_bic/choose_time.html',{"shop":shop})
+
+    def choose_use(request):
+        if request.method == "POST":
+            user_info = request.POST['user_info']
+            request.session['user_info'] = user_info
+            return render(request, 'imagine_bic/login.html',{"count":1})
+        return render(request, 'imagine_bic/choose_use.html',{"count":1})
+        
+
+    def notice(request):
+        notices = Notice.objects.all().order_by('num')
+        for notice in notices:
+            print(notice.subject)
+        return render(request, 'imagine_bic/notice.html', {'notices':notices})
+
+    def shop_detail(request,pk):
+        shop = get_object_or_404(Company, pk=pk)
+        id = request.session['id']
+        users = User.objects.filter(id = id)
+        if request.method == "POST":
+            request.session['company_num'] = pk
+            return render(request, 'imagine_bic/choose_time.html', {'shop': shop,'count':1})
+        return render(request, 'imagine_bic/shop_detail.html', {'shop': shop,'count':1, 'users':users})
+
+    def notice_detail(request,pk):
+        notice = get_object_or_404(Notice, pk=pk)
+        return render(request, 'imagine_bic/notice_detail.html', {'notice': notice})
+
+    def setting(request):
+        user_info = request.session['user_info']
+        if request.method == "POST":
+            return render(request, 'imagine_bic/setting_'+request.POST['setUrl']+".html", {"count":1,"user_info":user_info})
+        return render(request, 'imagine_bic/setting.html',{"user_info":user_info})
+
+    def setUrl(request, setUrl):
+        html = ""
+        if setUrl == "" :
+            html ='imagine_bic/setting.html'
+        elif setUrl == "modify":
+            html ='imagine_bic/setting_modify.html'
+        elif setUrl == "secession":
+            html = 'imagine_bic/setting_secession.html'
+        elif setUrl == "counsel":
+            html = 'imagine_bic/setting_counsel.html'
+        elif setUrl == "logout":
+            html = 'imagine_bic/setting_logout.html'
+        print(html)
+        return render(request, html)
+
+    def counsel(request):
+        id = request.session['id']
+        counsels = Counsel.objects.filter(member_id = id)
+        if request.method == "POST":
+            category = request.POST['category']
+            request.session['category'] = category
+            return render(request, 'imagine_bic/setting_write.html',{"count":1})
+        return render(request, 'imagine_bic/setting_counsel.html',{"counsels":counsels,"count":1})
+
+    def write(request):
+        if request.method == "POST":
+            category = request.session['category']
+            id = request.session['id']
+            subject = request.POST['subject']
+            content = request.POST['content']
+            counsel = Counsel.objects.create(member_id = id, subject = subject, content = content, regdate = datetime.datetime.now(), category = category) 
+            return render(request, 'imagine_bic/setting_write.html',{"count":2})
+        return render(request, 'imagine_bic/setting_write.html')
+
+    def check_list(request):
+        id = request.session['id']
+        historys = History.objects.filter(member_id = id)
+        return render(request, 'imagine_bic/course_rec.html',{"count":1,"historys":historys})
+    
+    def check_reservation(request, pk):
+        id = request.session['id']
+        history = get_object_or_404(History, history_num=pk)
+        if history.member_id == id:
+            companys = Company.objects.filter(company_num = history.company_num)
+            for company in companys:
+                return render(request, 'imagine_bic/reservation_check.html',{"history":history,"company":company})
+        return HttpResponse("<html><script>alert('잘못된 경로입니다.');location.href='index';</script></html>")
+
+    
--- imagine_bic/views.py
+++ imagine_bic/views.py
@@ -2,104 +2,36 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from django.http import HttpResponse, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
-from django.utils.translation import ugettext_lazy as _
-from django.utils import translation
 import datetime
 from .logic import *
 
-
-# # 번역 함수
-# # lan = request.COOKIES.get('lan') 뒤에 호출해서 사용 (두 개 세트로 사용해야 함)
-# def translate(request, lan):
-#     if lan == 'ko' or lan == 'en' or lan == 'jp':
-#         if translation.LANGUAGE_SESSION_KEY in request.session:
-#             del(request.session[translation.LANGUAGE_SESSION_KEY])
-#         translation.activate(lan)
-#         request.session[translation.LANGUAGE_SESSION_KEY] = f'{lan}'
-#     else:
-#         if translation.LANGUAGE_SESSION_KEY in request.session:
-#             del(request.session[translation.LANGUAGE_SESSION_KEY])
-#         translation.activate('en')
-#         request.session[translation.LANGUAGE_SESSION_KEY] = 'ko'
-
 def index(request):
-    lan = request.COOKIES.get('lan')
-    Sign.translate(request)
-    id = request.COOKIES.get('id') 
-    users = User.objects.filter(id = id)
-    for user in users :
-        user_info = user.info
-    if user_info == 0:
-        print("이 유저는 여행자")
-        response = render(request, 'imagine_bic/index.html', {'users':users,"count":1})
-        response.set_cookie('id',id)
-        return response
-    return render(request, 'imagine_bic/company_main.html',{'users':users,"count":1})
+    return Choose.index(request)
 
 @csrf_exempt
 def choose_lan(request):
-    if request.method == "POST":
-        return Choose.lan(request)
-    return render(request, 'imagine_bic/choose_lan.html')
+    return Choose.choose_lan(request)
 
 @csrf_exempt
 def choose_loc(request):
-    id = request.COOKIES.get('id') 
-    users = User.objects.filter(id = id)
-    if request.method == "POST":
-        request.session['company_loc']=request.POST['search']
-        return redirect('/choose_shop')
-    return render(request, 'imagine_bic/choose_loc.html',{"users":users})
+    return Choose.choose_loc(request)
 
 @csrf_exempt#,{"count":count}
 def choose_shop(request):
-    company_loc = request.session['company_loc'] 
-    companys = Company.objects.filter(company_loc = company_loc)
-    return render(request, 'imagine_bic/choose_shop.html',{"companys":companys})
+    return Choose.choose_shop(request)
 
 @csrf_exempt#,{"count":count}
 def choose_time(request):
-    print(request.COOKIES)
-    id = request.COOKIES.get('id') 
-    company_loc=request.COOKIES.get('company_loc')
-    company_num = request.COOKIES.get('company_num')
-    users = User.objects.filter(id = id)
-    for user in users:
-        name = user.name
-    print(company_num)
-    print(company_loc)
-    companys = Company.objects.filter(company_num = company_num)
-    for company in companys:
-        shop = company
-    if request.method == "POST":
-        print("post")
-        r_year = request.POST['r_year']
-        r_month = request.POST['r_month']
-        r_date = request.POST['r_date']
-        r_b = request.POST['r_btime']
-        r_r = request.POST['r_rtime']
-        r_btime = "{}-{}-{} {}".format(r_year,r_month,r_date,r_b)#datetime formatting
-        r_rtime = "{}-{}-{} {}".format(r_year,r_month,r_date,r_r)
-        btime = "1970-01-02 00:00:00"
-        rtime = "1970-01-02 00:00:00"
-        #db insert
-        history = History.objects.create(company_num=company_num, member_id = id, r_btime = r_btime, r_rtime = r_rtime, reserved_At = datetime.datetime.now(), btime =btime, rtime = rtime)
-        response = render(request, 'imagine_bic/choose_end.html',{"name":name,"history_num":history.history_num})
-        for company in companys:
-            Company.objects.update(rent_num=company.rent_num+1)
-        return response
-    print(shop)
-    return render(request, 'imagine_bic/choose_time.html',{"shop":shop})
+    return Choose.choose_time(request)
+    
 
 def choose_end(request):
     return render(request, 'imagine_bic/choose_end.html')
 
 @csrf_exempt
 def signup(request):
-    user_info =request.COOKIES.get('user_info')
+    user_info =request.session['user_info']
     if request.method == "POST":
-        # lan = request.COOKIES.get('lan')
-        # translate(request, lan)
         return Sign.signup(request,user_info)
     if user_info == "0":
         return render(request, 'imagine_bic/signup.html')
@@ -108,8 +40,6 @@ def signup(request):
 @csrf_exempt
 def signin(request):
     if request.method == "POST":
-        lan = request.COOKIES.get('lan')
-        Sign.translate(request)
         id = request.POST['id']
         pwd = request.POST['pwd']
         return Sign.signin(request,id,pwd)
@@ -131,134 +61,51 @@ def checkEmail(request):
 
 
 def choose_use(request):
-    if request.method == "POST":
-        user_info = request.POST['user_info']
-        response = render(requset, 'imagine_bic/choose_use.html')
-        response.set_cookie("user_info",user_info)
-        return response
-    return render(request, 'imagine_bic/choose_use.html',{"count":1})
+    return Choose.choose_use(request)
 
 def notice(request):
-    notices = Notice.objects.all().order_by('num')
-    for notice in notices:
-        print(notice.subject)
-    return render(request, 'imagine_bic/notice.html', {'notices':notices})
+    return Choose.notice(request)
 
 @csrf_exempt
 def shop_detail(request, pk):
-    shop = get_object_or_404(Company, pk=pk)
-    id = request.COOKIES.get('id') 
-    users = User.objects.filter(id = id)
-    if request.method == "POST":
-        response = render(request, 'imagine_bic/choose_time.html', {'shop': shop,'count':1})
-        response.set_cookie('company_num',pk)
-        print(response.cookies)
-        print(pk)
-        return response
-    return render(request, 'imagine_bic/shop_detail.html', {'shop': shop,'count':1, 'users':users})
+    return Choose.shop_detail(request,pk)
 
 def notice_detail(request, pk):
-    notice = get_object_or_404(Notice, pk=pk)
-    return render(request, 'imagine_bic/notice_detail.html', {'notice': notice})
+    return Choose.notice_detail(request,pk)
 
 @csrf_exempt
 def setting(request):
-    user_info = request.COOKIES.get('user_info')
-    if request.method == "POST":
-        return render(request, 'imagine_bic/setting_'+request.POST['setUrl']+".html", {"count":1,"user_info":user_info})
-    return render(request, 'imagine_bic/setting.html',{"user_info":user_info})
+    return Choose.setting(request)
 
 def setUrl(request, setUrl):
-    html = ""
-    if setUrl == "" :
-        html ='imagine_bic/setting.html'
-    elif setUrl == "modify":
-        html ='imagine_bic/setting_modify.html'
-    elif setUrl == "secession":
-        html = 'imagine_bic/setting_secession.html'
-    elif setUrl == "counsel":
-        html = 'imagine_bic/setting_counsel.html'
-    elif setUrl == "logout":
-        html = 'imagine_bic/setting_logout.html'
-    print(html)
-    return render(request, html)
+    return Choose.setUrl(request,setUrl)
 
 @csrf_exempt
 def counsel(request):
-    id = request.COOKIES.get('id')
-    counsels = Counsel.objects.filter(member_id = id)
-    if request.method == "POST":
-        category = request.POST['category']
-        response = render(request, 'imagine_bic/setting_write.html',{"count":1})
-        response.set_cookie("category",category)
-        return response
-    return render(request, 'imagine_bic/setting_counsel.html',{"counsels":counsels,"count":1})
+    return Choose.counsel(request)
 
 @csrf_exempt
 def write(request):
-    if request.method == "POST":
-        category = request.COOKIES.get('category')
-        id = request.COOKIES.get('id')
-        subject = request.POST['subject']
-        content = request.POST['content']
-        counsel = Counsel.objects.create(member_id = id, subject = subject, content = content, regdate = datetime.datetime.now(), category = category)
-        response = render(request, 'imagine_bic/setting_write.html',{"count":2})
-        return response
-    return render(request, 'imagine_bic/setting_write.html')
+    return Choose.write(request)
 
 def map(request):
     return render(request, 'imagine_bic/map.html')
 
 @csrf_exempt
 def check_list(request):
-    id = request.COOKIES.get('id')
-    historys = History.objects.filter(member_id = id)
-    return render(request, 'imagine_bic/course_rec.html',{"count":1,"historys":historys})
+    return Choose.check_list(request)
 
 @csrf_exempt
 def check_reservation(request, pk):
-    id = request.COOKIES.get('id')
-    history = get_object_or_404(History, history_num=pk)
-    if history.member_id == id:
-        companys = Company.objects.filter(company_num = history.company_num)
-        for company in companys:
-            return render(request, 'imagine_bic/reservation_check.html',{"history":history,"company":company})
-    return HttpResponse("<html><script>alert('잘못된 경로입니다.');location.href='index';</script></html>")
-
-
-def choose_use(request):
-    if request.method == "POST":
-        user_info = request.POST['user_info']
-        response = render(request, 'imagine_bic/login.html',{"count":1})
-        response.set_cookie('user_info',user_info)
-        return response
-    return render(request, 'imagine_bic/choose_use.html',{"count":1})
-
+    return Choose.check_reservation(request, pk)
 
 @csrf_exempt
 def signup_company(request):
-    user_info =request.COOKIES.get('user_info')
-    id = request.COOKIES.get('id')
-    users = User.objects.filter(id = id)
-    for user in users:
-        name = user.name
-    if request.method == "POST":
-        company_addr = request.POST['company_addr']
-        company_loc = request.POST['company_loc']
-        company_phone = request.POST['company_phone']
-        bicycle_num = request.POST['bicycle_num']
-        s_business = request.POST['s_business']
-        e_business = request.POST['e_business']
-        company_info = request.POST['company_info']
-        company = Company.objects.create(member_name = name, company_addr = company_addr,company_phone=company_phone,bicycle_num=bicycle_num,member_id=id, s_business=s_business,e_business=e_business,company_info=company_info,company_loc=company_loc,rent_num=0)
-        response = render(request, 'imagine_bic/login.html',{"count":1})
-        response.delete_cookie('id')
-        return response
-    return render(request, 'imagine_bic/signup2_company.html')
+    return Sign.signup_company(request)
 
 
 def company_main(request):
-    id = request.COOKIES.get('id')
+    id = request.session['id']
     users = User.objects.filter(id = id)
     return render(request, 'imagine_bic/company_main.html',{"count":1,"users":users})
 
