debd2b3076d58e4a708c6865d883b22cd9829f1d
Sagittta <js1verj@gmail.com>
2020-05-23 21:18:51 +0900

    password check, create submit button, blank check

--- imagine_bic/static/js/signup1.js
+++ imagine_bic/static/js/signup1.js
@@ -6,87 +6,114 @@ function chfun(){
     location.href="/signin";
 }
 
-function sm(){
+function sm() {
     joinForm.submit();
 }
-        /* 중복체크! */
-        $(function(){
-            /* 아이디 수정했을 때 */
-            $('#id').change(function () {
-                $('#barEmailck').show();
-            });
 
-            // 중복확인 버튼을 눌렀을 때
-            $('#barEmailck').click(function(){
-                var id = $('#id').val();
-                // 이메일 란이 공백일 경우
-                if(id == ''){
-                    alert('이메일을 입력해주세요.');
-                    return false;
+$(function () {
+    var id = $("#id").val();
+    var name = $("#name").val();
+    var pwd = $("#pwd").val();
+
+    if (id === "" || name === "" || pwd === "") {
+        $("#joinBt").attr("disabled", "disabled");
+    }
+});
+
+/* 중복체크! */
+$(function () {
+    /* 아이디 수정했을 때 */
+    $('#id').change(function () {
+        $('#barEmailck').show();
+        $("#joinBt").attr("id_check_result", "fail");
+        $("#joinBt").attr("disabled", "disabled");
+    });
+
+    // 중복확인 버튼을 눌렀을 때
+    $('#barEmailck').click(function () {
+        var id = $('#id').val();
+        // 이메일 란이 공백일 경우
+        if (id == '') {
+            alert('이메일을 입력해주세요.');
+            $("#joinBt").attr("id_check_result", "fail");
+            $("#joinBt").attr("disabled", "disabled");
+            return false;
+        }
+        // debugger;
+        $.ajax({
+            url: 'check_email',
+            data: {
+                id: id
+            },
+            type: 'get',
+            dataType: 'json',
+            success: function (response) {
+                if (response.result !== 'success') {
+                    console.error(response.data)
+                    return;
                 }
-                // debugger;
-                $.ajax({
-                    url:'check_email',
-                    data: {
-                        id: id
-                    },
-                    type:'get',
-                    dataType:'json',
-                    success:function(response){
-                        if(response.result != 'success'){
-                            console.error(response.data)
-                            return;
-                        }
-                        if(response.data == 'exist'){
-                            alert("존재하는 이메일 입니다!");
-                            $('#id').val('').focus();
-                            return;
-                        } else {
-                            alert("사용 가능한 이메일입니다!");
-                            $("#joinBt").attr("id_check_result", "success");
-                            return;
-                        }
-                        // console.log(response)
-                    },
-                    error : function(xhr, error){
-                        alert("서버와의 통신에서 문제가 발생했습니다.");
-                        console.error("error : " + error);
-                    }
-                });
-
-                return false;
-            });
-
-            // 가입 버튼 눌렀을 때
-            $('#joinForm').submit(function() {
-                if($("#joinBt").attr("id_check_result") == "fail") {
-                    alert("이메일 중복 확인를 해주시기 바랍니다.");
-                    $("#id").focus();
-                    return false;
+                if (response.data === 'exist') {
+                    alert("존재하는 이메일 입니다!");
+                    $('#id').val('').focus();
+                    $("#joinBt").attr("id_check_result", "fail");
+                    $("#joinBt").attr("disabled", "disabled");
+                    return;
+                } else {
+                    alert("사용 가능한 이메일입니다!");
+                    $("#joinBt").attr("id_check_result", "success");
+                    $("#joinBt").removeAttr("disabled");
+                    $('#barEmailck').hide();
+                    return;
                 }
-            });
-
-            $("#id").on("propertychange change keyup paste input", function(){
-                $('#barEmailck').show();
+                // console.log(response)
+            },
+            error: function (xhr, error) {
+                alert("서버와의 통신에서 문제가 발생했습니다.");
                 $("#joinBt").attr("id_check_result", "fail");
-            });
+                $("#joinBt").attr("disabled", "disabled");
+                console.error("error : " + error);
+            }
         });
 
-        // 비밀번호 확인
-        $(function(){
-            $("input").keyup(function(){ 
-                var pwd = $("#pwd").val(); 
-                var pwdCk = $("#pwdck").val(); 
-                if(pwd != "" || pwdCk != ""){ 
-                    if(pwd == pwdCk){ 
-                        $("#alert-success").show(); 
-                        $("#alert-danger").hide(); 
-                        $("#joinBt").removeAttr("disabled"); 
-                    } else{ 
-                        $("#alert-success").hide(); 
-                        $("#alert-danger").show(); 
-                        $("#joinBt").attr("disabled", "disabled"); 
-                    } 
-                } 
-            }); 
-        });
+        return false;
+    });
+
+    // 가입 버튼 눌렀을 때
+    $('#joinForm').submit(function () {
+        console.log($("#joinBt").attr("id_check_result"));
+        if ($("#joinBt").attr("id_check_result") === "fail") {
+            alert("이메일 중복 확인를 해주시기 바랍니다.");
+            $("#joinBt").attr("disabled", "disabled");
+            $("#id").focus();
+            return false;
+        }
+    });
+
+    $("#id").on("propertychange change keyup paste input", function () {
+        $('#barEmailck').show();
+        $("#joinBt").attr("id_check_result", "fail");
+    });
+});
+
+// 비밀번호 확인
+$(function () {
+    $("input").keyup(function () {
+        var pwd = $("#pwd").val();
+        var pwdCk = $("#pwdck").val();
+        if (pwd !== "" || pwdCk !== "") {
+            if (pwd === pwdCk) {
+                $("#alert-success").show();
+                $("#alert-danger").hide();
+                $("#joinBt").removeAttr("disabled");
+            } else {
+                $("#alert-success").hide();
+                $("#alert-danger").show();
+                $("#joinBt").attr("disabled", "disabled");
+            }
+        } else {
+            $("#alert-success").hide();
+            $("#alert-danger").show();
+            $("#joinBt").attr("disabled", "disabled");
+        }
+    });
+});
