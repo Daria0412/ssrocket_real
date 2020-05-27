25e7ce09a2a30127969e563b00dccb8fa2210371
daria0412 <s2018w29@icloud.com>
2020-05-28 06:51:34 +0900

    modify choose_loc logic

--- imagine_bic/static/js/choose_loc1.js
+++ imagine_bic/static/js/choose_loc1.js
@@ -5,12 +5,7 @@ function hfun(){
     location.href="/index";
 }
 
-<<<<<<< HEAD:imagine_bic/static/js/choose_loc1.js
 function sm(){
     alert("HELLO");
-=======
-function sm(company_loc){
-    document.getElementById("search").value = company_loc;
->>>>>>> ebd9a93655c08be44186181701d919bb2c3408eb:imagine_bic/static/js/choose_loc.js
     lanForm.submit();
 }
