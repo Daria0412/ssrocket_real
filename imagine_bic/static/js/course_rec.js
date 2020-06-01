fa9caaf3d24b2a2e7caa8fbf1ea2906005e0cc12
South_under_been <s2018s27@e-mirim.hs.kr>
2020-06-01 09:37:12 +0900

    res_chk(bt_see_all)

---
+++ imagine_bic/static/js/course_rec.js
@@ -0,0 +1,14 @@
+function bkfun(){
+    history.back();
+}
+
+function a(){
+    var el = document.getElementById("a");
+    el.style.display='block';
+    
+}
+
+function b(){
+    var el1=document.getElementById("a");
+    el1.style.display='none';
+}
